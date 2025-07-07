25b61c917b23462e145dbd30c262f58182929410
project2479 <jaywrsnp6@gmail.com>
2025-07-07 22:09:24 +0800

    new

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";import { Link } from "react-router-dom";import Dashboard from "./Dashboard";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
 import Pwd from "./Pwd";
 import Seniors from "./Seniors";
 import Infrastructure from "./Infrastructures";
@@ -77,18 +79,6 @@ function Admin() {
 								</Link>
 							</li>
 						</ul>
-
-						<div className="flex flex-wrap items-center cursor-pointer border-t border-gray-200 py-2 mt-6">
-							<img
-								src="https://readymadeui.com/team-2.webp"
-								className="w-10 h-10 rounded-md border-2 border-white"
-								alt="User"
-							/>
-							<div className="ml-4">
-								<p className="text-sm text-slate-900 font-medium">John Doe</p>
-								<p className="text-xs text-slate-500 mt-0.5">Admin User</p>
-							</div>
-						</div>
 					</div>
 				</div>
 			</nav>
